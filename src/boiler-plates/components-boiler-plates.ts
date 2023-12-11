15b35cfed15eda4fc3ec6d6d0b7f09d167881850
AshrafChitamba <bsc-40-20@unima.ac.mw>
2023-12-11 19:25:41 +0200

    c-components-boilerplates

--- src/boiler-plates/components-boiler-plates.ts
+++ src/boiler-plates/components-boiler-plates.ts
@@ -1,20 +1,20 @@
 // SolidJs boiler-plate
 export const solidBoilerPlate = (componentName: string) => {
   return `import { Component } from "solid-js";
-        \nexport const ${componentName}: Component = () => {\n\treturn <div>${componentName}</div>;\n};
+        \nexport const ${componentName}: Component = () => {\n\treturn (\n\t\t<div>${componentName}</div>\n\t);\n};
     `;
 };
 
 // ReactJs boiler-plate
 export const reactBoilerPlate = (componentName: string) => {
   return `import { FC } from 'react';
-       \nexport const ${componentName}: FC = () => {\n\treturn <div>${componentName}</div>;\n};
+       \nexport const ${componentName}: FC = () => {\n\treturn (\n\t\t<div>${componentName}</div>\n\t);\n};
     `;
 };
 
 // ReactJs boiler-plate
 export const reactNativeBoilerPlate = (componentName: string) => {
   return `import { View, Text } from 'react-native';
-        \nexport const ${componentName} = () => {\n\treturn <View>\n\t\t<Text>${componentName}</Text>\n\t</View>;\n};
+        \nexport const ${componentName} = () => {\n\treturn (\n\t\t<View>\n\t\t\t<Text>${componentName}</Text>\n\t\t</View>\n\t);\n};
     `;
 };
